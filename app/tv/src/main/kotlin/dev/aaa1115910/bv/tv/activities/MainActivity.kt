eda8f374b770c30374d08ef8064e1d4c8751e8d2
yhan1203 <47243469+yhan1203@users.noreply.github.com>
2026-06-10 14:59:17 +0800

    fix: 添加 MutableState 委托写操作所需的 setValue import
    Kotlin 2.2+ 需要显式导入 setValue 才能使用
    'var ... by remember { mutableStateOf(...) }' 的写操作委托

--- app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/activities/MainActivity.kt
+++ app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
